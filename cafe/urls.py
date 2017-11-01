ede882fe7c4c6d9d51952ec6589e036c81bc1f83
mohammad <mohammadrezashamsashkezary@gmail.com>
2017-11-01 18:43:17 +0330

    ohm

--- cafe/urls.py
+++ cafe/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', LoginView.as_view(), name='login'),
-    url(r'index/^$', HomePageView.as_view(), name='index'),
+    url(r'index/$', HomePageView.as_view(), name='index'),
     url(r'^signup/$', UserInfoView.as_view(), name='signup'),
     # url(r'^$', views.index, name='index'),
 ]
