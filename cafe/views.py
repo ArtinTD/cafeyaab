c3ead2a18b61e456fbbabd3a2a53107178f2810f
mohammad <mohammadrezashamsashkezary@gmail.com>
2017-11-01 15:23:24 +0330

    recaptch

--- cafe/views.py
+++ cafe/views.py
@@ -5,6 +5,16 @@ from django.views.generic.base import TemplateView, RedirectView
 from django.views.generic.edit import FormView
 from cafe.forms.user_forms import UserInfoForm
 from cafe.models import UserInfo
+import json
+import urllib
+
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.contrib import messages
+
+# from .models import Comment
+# from .forms import CommentForm
+
 
 class HomePageView(FormView):
     success_url = '/index/'
@@ -51,3 +61,36 @@ class UserInfoView(LoginRequiredMixin, FormView):
             user_info.username = form.data['username']
             user_info.save()
         return super(UserInfoView, self).form_valid(form)
+
+
+def comments(request):
+    comments_list = UserInfo.objects.order_by('-created_at')
+
+    if request.method == 'POST':
+        form = UserInfoForm(request.POST)
+        if form.is_valid():
+
+            ''' Begin reCAPTCHA validation '''
+            recaptcha_response = request.POST.get('g-recaptcha-response')
+            url = 'https://www.google.com/recaptcha/api/siteverify'
+            values = {
+                'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
+                'response': recaptcha_response
+            }
+            data = urllib.parse.urlencode(values).encode()
+            req =  urllib.request.Request(url, data=data)
+            response = urllib.request.urlopen(req)
+            result = json.loads(response.read().decode())
+            ''' End reCAPTCHA validation '''
+
+            if result['success']:
+                form.save()
+                messages.success(request, 'New comment added with success!')
+            else:
+                messages.error(request, 'Invalid reCAPTCHA. Please try again.')
+
+            return redirect('comments')
+    else:
+        form = UserInfoForm()
+
+    return render(request, 'core/comments.html', {'comments': comments_list, 'form': form})
