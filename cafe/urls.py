aaef4d70c3d712ff944a2e9e4e83b4abcf35f8f8
mohammad <mohammadrezashamsashkezary@gmail.com>
2017-11-02 14:02:41 +0330

    cafe

--- cafe/urls.py
+++ cafe/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from django.contrib.auth.views import login, logout
-from cafe.views import HomePageView
+from cafe.views import HomePageView, CafeView
 from . import views
 
 urlpatterns = [
     # url(r'login/$', LoginView.as_view(), name='login'),
     url(r'^index/$', HomePageView.as_view(), name='index'),
+    url(r'^cafe/$', CafeView.as_view(), name='cafe'),
     url(r'^login', login, name='login'),
     url(r'^logout', logout, name='logout'),
     # url(r'^$', SignupView.as_view(), name='signup'),
